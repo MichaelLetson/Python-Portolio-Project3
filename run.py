8c23bdb3ae4e7570a2d40e96b2d4659947b63e2d
MichaelLetson <mletsongamble@gmail.com>
2022-03-12 18:18:47 +0000

    Create function validate_data to validate the users input

--- run.py
+++ run.py
@@ -14,13 +14,35 @@ SHEET = GSPREAD_CLIENT.open('team_data')
 
 def collect_data():
     """
-    collects data input by the user
+    collects data input by the user, and if valid, returns confirmation of data. 
     """
-    print('Please enter each teams data from the last calendar month, starting with team 1.')
-    print("Don't forget to separate each piece of data with a comma.")
-    print("For example: 25, 42, 44, 15, 25\n")
+    while True:
+        print('Please enter each teams data from the last calendar month, starting with team 1.')
+        print("Don't forget to separate each piece of data with a comma.")
+        print("For example: 25,42,44,15,25\n")
 
-    user_data = input('Enter data here: ')
-    print(f'Confirmation of data: {user_data}')
+        user_data = input('Enter data here: ')
+        input_data = user_data.split(',')
+    
+        if validate_data(input_data):
+            print(f'Confirmation of data: {input_data}')
+            break
+
+def validate_data(values):
+    """
+    Converts values into integers. 
+    Raising ValueError if values cannot be converted or there are not exactly 5 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 5:
+            raise ValueError(
+                f'Exactly 5 values are required.'
+            )
+    except ValueError as error:
+        print(f'Invalid entry: {error} - Please try again!')
+        return False
+    
+    return True
 
 collect_data()
